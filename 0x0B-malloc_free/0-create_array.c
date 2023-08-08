5449d8c196ae8d5050dbe628a7c8cbee67ce0acf
SophieKamilya <kamilyasophie@gmail.com>
2023-08-08 08:05:58 -0700

    helio

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -26,38 +26,7 @@ char *create_array(unsigned int size, char c)
 			pt[i] = c;
 			i++;
 		}
-		pt[size] = '\0';
-	}
-	return (pt);
-#include "main.h"
-
-/**
- *  * create_array - creates an array of chars with specific char
- *   * @size:unsigned int
- *    * @c:char
- *     * Return:string
- *      */
-
-char *create_array(unsigned int size, char c)
-{
-	char *pt;
-	unsigned int i = 0;
-
-	if (size == 0)
-	{
-			pt = NULL;
-	}
-	else
-	{
-			pt = malloc(size);
-			if (pt == NULL)
-				return (NULL);
-			while (i < size)
-			{
-						pt[i] = c;
-						i++;
-			}
-			pt[size] = '\0';
+		pt[size] ='\0';
 	}
 	return (pt);
 }
